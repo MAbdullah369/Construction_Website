35e85ebbf8e7fabec7fd673fa5bc042b644c0d83
MAbdullah369 <bscs22111@itu.edu.pk>
2025-02-24 03:29:25 +0500

    Header-added

--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <p>
-        Header 
-    </p>
+    <div className='header'>
+      <img src="img.png" alt="" />
+      <p>Rawalpindi | Lahore | Karachi</p>
+      <img src="img2.png" alt="" />
+      <p>+92 303 42232979</p>
+      <img src="img4.png" alt="" />
+      <p>+92 331 4874500</p>
+      <img src="img1.png" alt="" />
+      <p> zahidabdullah161@gmail.com</p>
+    </div>
   )
 }
 
