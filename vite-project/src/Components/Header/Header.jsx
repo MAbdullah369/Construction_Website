d82342a425bf2bed6e3101365cd4c685f051b829
HROON157 <haroonshafiq104@gmail.com>
2025-02-24 23:12:22 +0500

    new Header added

--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -1,20 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
-import './Header.css'
-
-const Header = () => {
-  return (
-    <div className='header'>
-      <img src="img.png" alt="" />
-      <p>Rawalpindi | Lahore | Karachi</p>
-      <img src="img2.png" alt="" />
-      <p>+92 303 42232979</p>
-      <img src="img4.png" alt="" />
-      <p>+92 331 4874500</p>
-      <img src="img1.png" alt="" />
-      <p> zahidabdullah161@gmail.com</p>
-    </div>
-=======
 import TopNavbar from './TopNavbar/TopNavbar'
 import Navbar from './Navbar/Navbar'
 const Header = () => {
@@ -23,7 +7,6 @@ const Header = () => {
     <TopNavbar/>
     <Navbar/>
     </>
->>>>>>> 4562da246c627435f1b726f1014fe04bfa373b56
   )
 }
 
