bf7c633556cb41ff47c2acd9e7ef8a8e80db6b3f
MAbdullah369 <bscs22111@itu.edu.pk>
2025-03-01 13:49:06 +0500

    correction

--- vite-project/src/Components/Header/Header.jsx
+++ vite-project/src/Components/Header/Header.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import './Header.css'
-
-const Header = () => {
-  return (
-    <div className='header'>
-      <img src="img.png" alt="" />
-      <p>Rawalpindi | Lahore | Karachi</p>
-      <img src="img2.png" alt="" />
-      <p>+92 303 42232979</p>
-      <img src="img4.png" alt="" />
-      <p>+92 331 4874500</p>
-      <img src="img1.png" alt="" />
-      <p> zahidabdullah161@gmail.com</p>
-    </div>
-
-  )
-}
-=======
 import React from "react";
 import TopNavbar from "./TopNavbar/TopNavbar";
 import Navbar from "./Navbar/Navbar";
@@ -29,6 +9,6 @@ const Header = () => {
     </>
   );
 };
->>>>>>> 1854c389d382e2fd071884458fd219f8aa5c2207
+
 
 export default Header;
